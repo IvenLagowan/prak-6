022872721eb5788ae3ee9cadef72edffd3bf01d2
Your Name <you@example.com>
2022-11-23 22:33:21 -0800

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('.pic a').lightBox({
+    
+    imageLoading: 'lightbox/images/loading2.gif',
+    // imageBtnClose: 'lightbox/images/close.jpg', imageBtnPrev: 'lightbox/images/prev.gif', imageBtnNext: 'lightbox/images/next.gif'
+    
+    });
+    
+    });
+    
+    
